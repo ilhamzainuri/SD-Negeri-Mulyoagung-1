e9fd6b5bda8542bf30c826ebb1db513273dc397f
YEZZY <hafizsengkaling@gmail.com>
2026-08-11 10:16:46 +0700

    menambahkan database

--- backend/db_sdn1mulyoagung.sql
+++ backend/db_sdn1mulyoagung.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2026 at 06:10 AM
+-- Generation Time: Aug 11, 2026 at 05:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -64,8 +64,8 @@ CREATE TABLE `fasilitas` (
 
 INSERT INTO `fasilitas` (`id`, `judul`, `deskripsi`, `foto`) VALUES
 (1, 'Laboratorium Komputer & TIK Interaktif', 'Dilengkapi 30 unit komputer terkini, jaringan Wi-Fi sekolah, dan Smart Display untuk pembelajaran coding dasar & literasi digital.', 'https://images.unsplash.com/photo-1516321318423-f06f85e504b3?auto=format&fit=crop&q=80&w=600'),
-(2, 'Perpustakaan \"Taman Ilmu\"', 'Koleksi ribuan buku cerita, modul pembelajaran, koleksi literasi digital e-book, dan sudut baca ramah anak yang nyaman.', 'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?auto=format&fit=crop&q=80&w=600'),
-(3, 'Lapangan Olahraga & Upacara', 'Areal seluas 800m² dilapisi plester berkualitas untuk upacara bendera, senam bersama, bulutangkis, basket, dan futsal.', 'https://images.unsplash.com/photo-1574629810360-7efbbe195018?auto=format&fit=crop&q=80&w=600'),
+(2, 'Perpustakaan', 'Koleksi ribuan buku cerita, modul pembelajaran, koleksi literasi digital e-book, dan sudut baca ramah anak yang nyaman.', 'backend/uploads/fasilitas/1786413887_perpus1.jpeg'),
+(3, 'Lapangan Olahraga & Upacara', 'Areal seluas 800m² dilapisi plester berkualitas untuk upacara bendera, senam bersama, bulutangkis, basket, dan futsal.', 'backend/uploads/fasilitas/1786416364_lapangan.jpeg'),
 (4, 'Ruang UKS & Poliklinik Sekolah', 'Fasilitas pertolongan pertama kesehatan dengan tempat tidur bersih, pengukuran TB/BB rutin, dan kerja sama Puskesmas Dau.', 'https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?auto=format&fit=crop&q=80&w=600'),
 (5, 'Kantin Sehat Bergizi', 'Menyediakan makanan dan minuman sehat yang higienis, bebas bahan pengawet berbahaya, dan diawasi oleh tim gizi sekolah.', 'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?auto=format&fit=crop&q=80&w=600');
 
@@ -116,17 +116,17 @@ INSERT INTO `guru_tendik` (`id`, `nama`, `nip`, `jabatan`, `tugas`, `foto`, `riw
 (6, 'ZAINURI, M.Pd.', NULL, 'Guru Mata Pelajaran', 'Pendidikan Agama Islam', 'backend/uploads/guru/1786071170_logoma.jpg', 'S2  (Pendidikan Guru Sekolah Dasar)', 'Laki-laki', 'Aktif', 'p'),
 (7, 'WEGA BAGUS SETIAWAN, S.Or., M.Pd., Gr.', NULL, 'Guru Mata Pelajaran', 'Pendidikan Jasmani Dan Rohani', 'backend/uploads/guru/1786071266_logoma.jpg', 'S1 PJOK', 'Laki-laki', 'Aktif', 'p'),
 (8, 'FANDI ARI WIJAYA, S.Or., Gr.', NULL, 'Guru Mata Pelajaran', 'Pendidikan Jasmani Dan Rohani', 'backend/uploads/guru/1786071327_logoma.jpg', 'S1 PJOK', 'Laki-laki', 'Aktif', 'p'),
-(9, 'SUNU HAYUTAMA, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 1A', 'backend/uploads/guru/1786071419_logoma.jpg', 'S1  (Pendidikan Guru Sekolah Dasar)', 'Perempuan', 'Aktif', 'p'),
-(10, 'PUTRI ANGGUN LIARTA, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 1B', 'backend/uploads/guru/1786071504_logoma.jpg', 'S1', 'Perempuan', 'Aktif', 'p'),
-(11, 'RATHA YULIYA KIRNAWATI, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 2A', 'backend/uploads/guru/1786071592_logoma.jpg', 'S1', 'Perempuan', 'Aktif', 'p'),
+(9, 'SUNU HAYUTAMA, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 1A', 'backend/uploads/guru/1786339856_Screenshot 2026-08-10 123039.png', 'S1  (Pendidikan Guru Sekolah Dasar)', 'Perempuan', 'Aktif', 'p'),
+(10, 'PUTRI ANGGUN LIARTA, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 1B', 'backend/uploads/guru/1786339889_Screenshot 2026-08-10 123120.png', 'S1', 'Perempuan', 'Aktif', 'p'),
+(11, 'RATNA YULIYA KIRNAWATI, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 2A', 'backend/uploads/guru/1786339925_Screenshot 2026-08-10 123152.png', 'S1', 'Perempuan', 'Aktif', 'p'),
 (12, 'YUNIA NUR AFIYAH, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 2B', 'backend/uploads/guru/1786071638_logoma.jpg', 'S1', 'Perempuan', 'Aktif', 'p'),
 (13, 'ADI KURNIAWAN, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 3A', '', 'S1', 'Laki-laki', 'Aktif', 'p'),
 (14, 'SOQIBATUL ISLAMIYAH, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 3B', '', 'S1', 'Perempuan', 'Aktif', 'p'),
 (15, 'NUR AINI FARIDA, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 4A', '', 'S1', 'Perempuan', 'Aktif', 'p'),
-(16, 'SRI HARTATIK, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 4B', '', 'S1', 'Perempuan', 'Aktif', 'p'),
+(16, 'SRI HARTATIK, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 4B', 'backend/uploads/guru/1786340232_Screenshot 2026-08-10 123645.png', 'S1', 'Perempuan', 'Aktif', 'p'),
 (17, 'SITI MAISAROH, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 5A', '', 'S1', 'Perempuan', 'Aktif', 'p'),
 (18, 'YUNI TRI HARIANTI, S.IP., S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 5B', '', 'S1', 'Perempuan', 'Aktif', 'p'),
-(19, 'VIVIN NOHTAHFIAH, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 6A', '', 'S1', 'Perempuan', 'Aktif', 'p'),
+(19, 'VIVIN NOHTAHFIAH, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 6A', 'backend/uploads/guru/1786340264_Screenshot 2026-08-10 123732.png', 'S1', 'Perempuan', 'Aktif', 'p'),
 (20, 'Yulida Ariani, S.Pd.', NULL, 'Guru Wali Kelas', 'Guru Kelas 6B', '', 'S1', 'Perempuan', 'Aktif', 'p'),
 (21, 'ABDUL MUJIB', NULL, 'Tenaga Kependidikan', 'Penjaga', '', 'S1', 'Laki-laki', 'Aktif', 'p'),
 (22, 'MARSUDI', NULL, 'Tenaga Kependidikan', 'Tenaga Kebersihan', '', 'S1', 'Laki-laki', 'Aktif', 'p'),
@@ -169,15 +169,17 @@ CREATE TABLE `pengumuman_penting` (
   `show_photo` tinyint(1) NOT NULL DEFAULT 0,
   `foto` varchar(255) DEFAULT NULL,
   `photo_link` varchar(255) DEFAULT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT 1
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `tanggal_mulai` date DEFAULT NULL,
+  `tanggal_selesai` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pengumuman_penting`
 --
 
-INSERT INTO `pengumuman_penting` (`id`, `judul`, `isi`, `running_text`, `show_popup`, `show_button`, `button_text`, `button_link`, `show_photo`, `foto`, `photo_link`, `is_active`) VALUES
-(1, 'Pengumuman Penting', 'Sistem Penerimaan Murid Baru (SPMB) Tahun Ajaran Baru telah dibuka! Jangan lewatkan kesempatan untuk bergabung dengan sekolah dasar terbaik di Kota Malang. Kuota terbatas untuk gelombang pertama.', 'Pendaftaran Siswa Baru (PPDB) Tahun Ajaran Baru Resmi Dibuka! Segera Daftarkan Putra-Putri Anda.', 1, 1, 'SPMB', 'https://sdn1mulyoagung.sch.id/ppdb', 0, '', '', 1);
+INSERT INTO `pengumuman_penting` (`id`, `judul`, `isi`, `running_text`, `show_popup`, `show_button`, `button_text`, `button_link`, `show_photo`, `foto`, `photo_link`, `is_active`, `tanggal_mulai`, `tanggal_selesai`) VALUES
+(1, 'Pengumuman Penting', 'Sistem Penerimaan Murid Baru (SPMB) Tahun Ajaran Baru telah dibuka! Jangan lewatkan kesempatan untuk bergabung dengan sekolah dasar terbaik di Kota Malang. Kuota terbatas untuk gelombang pertama.', 'Pendaftaran Siswa Baru (PPDB) Tahun Ajaran Baru Resmi Dibuka! Segera Daftarkan Putra-Putri Anda.', 1, 1, 'SPMB', 'https://sdn1mulyoagung.sch.id/ppdb', 1, 'backend/uploads/pengumuman/1786411305_Screenshot 2026-08-11 082132.png', '', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -197,7 +199,7 @@ CREATE TABLE `sambutan_kepsek` (
 --
 
 INSERT INTO `sambutan_kepsek` (`id`, `nama`, `sambutan`, `foto`) VALUES
-(1, 'Amalia Dyah Erviana, S.Pd.', 'Assalamualaikum Wr. Wb.\r\n\r\nSelamat datang di website resmi SD Negeri Mulyoagung 1. Kami berkomitmen memberikan pendidikan terbaik bagi putra-putri Anda, membimbing mereka menjadi generasi yang tidak hanya cerdas secara akademik, namun juga memiliki karakter dan budi pekerti yang luhur. \r\n\r\nMelalui semangat kebersamaan, inovasi pembelajaran berbasis digital, dan penguatan Profil Pelajar Pancasila, kami yakin dapat membentuk peserta didik yang siap menghadapi tantangan masa depan dengan tetap memegang teguh nilai-nilai keagamaan dan budaya bangsa. Melalui website ini, kami berharap dapat menjalin komunikasi yang lebih erat dengan seluruh masyarakat dan orang tua wali murid.', 'backend/uploads/sambutan/1786324881_kepsek.jpg'))));
+(1, 'Amalia Dyah Erviana, S.Pd.', 'Assalamu\'alaikum Wr. Wb.\r\n\r\nSelamat datang di website resmi SD Negeri Mulyoagung 1. Kami berkomitmen memberikan pendidikan terbaik bagi putra-putri Anda, membimbing mereka menjadi generasi yang tidak hanya cerdas secara akademik, namun juga memiliki karakter dan budi pekerti yang luhur. \r\n\r\nMelalui semangat kebersamaan, inovasi pembelajaran berbasis digital, dan penguatan Profil Pelajar Pancasila, kami yakin dapat membentuk peserta didik yang siap menghadapi tantangan masa depan dengan tetap memegang teguh nilai-nilai keagamaan dan budaya bangsa. Melalui website ini, kami berharap dapat menjalin komunikasi yang lebih erat dengan seluruh masyarakat dan orang tua wali murid.', 'backend/uploads/sambutan/1786324881_kepsek.jpg');
 
 -- --------------------------------------------------------
 
@@ -325,7 +327,7 @@ ALTER TABLE `fasilitas`
 -- AUTO_INCREMENT for table `galeri`
 --
 ALTER TABLE `galeri`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `guru_tendik`
